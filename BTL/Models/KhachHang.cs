34312d616ec6c64c99ea0229305c400799ab1870
Nguyen Thi Phuong Thao <phuongthaoov0408@gmail.com>
2021-06-06 09:13:59 +0700

    Add project files.

---
+++ BTL/Models/KhachHang.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BTL.Models
+{
+    [Table("KhachHangs")]
+    public class KhachHang
+    {
+        [Key]
+        public string KhachHangIB { get; set; }
+        public string TenKhachHang { get; set; }
+        public string Diachi { get; set; }
+        
+    }
+}
